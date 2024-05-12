31138107fd04ebd29d1052322d3bc3eecf73a470
indianaea <126889057+indianaea@users.noreply.github.com>
2024-05-12 18:25:26 +0000

    Byrjun á navbar
    footer, homescreen and navbar added under compontents

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,14 @@
 "use client";
 import Image from "next/image";
 /*import Navbar from './components/layout/header'; // Make sure the path to Navbar is correct*/
-import Routers from "./routers";
-import Dashboard from "./components/homeScreen/page";
+import Header from "./components/navbar/header";
 
 export default function Home() {
   /*return <h1>Hello, Home page!</h1>*/
   return (
     <>
-      {/* <TeamClock /> */}
-      {/* <TeamObject /> */}
-      {/* <GameReviews /> */}
-      {/* <ShoppingList /> */}
-      {/* <Expenses />*/}
-      {/*<Table />*/}
-      {/* <CompositeTable /> */}
-      <Routers />
+      {/*<Routers />*/}
+      <Header />
     </>
   );
 
