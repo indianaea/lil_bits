80ec7502e90ec9976753d61c9dd256a88c49a22e
indianaea <126889057+indianaea@users.noreply.github.com>
2024-05-23 20:04:17 +0000

    laga navbar v/ routing

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -4,31 +4,33 @@ import "./navbar.css"
 import Link from "next/link";
 
 
+export const NavbarMenu = () => {
+    return (
+        <div className="container">
+            <Link href="/" className="logo">
+                <img src="lilbits.png" alt="Website Logo" />
+            </Link>
+            <ul className="desktopMenu">
+                <li>
+                    <Link href="/order">Order dish</Link>
+                </li>
+                <li>
+                    <Link href="/aboutUs">About us</Link>
+                </li>
+                <li>
+                    <Link href="/menu">Menu</Link>
+                </li>
+            </ul>
+        </div>
+    );
+};
+
 const Navbar = () => {
     return (
-      <div className="navbar">
-        <div className="container">
-          <div className="logo">
-            <img src="lilbits.png" alt="Logo" />
-          </div>
-          <div className="desktopMenu">
-            <a href="#home">Home</a>
-            <a href="#services">Services</a>
-            <a href="#about">About</a>
-            <a href="#contact">Contact</a>
-          </div>
-          <div className="menuBtn">
-            {/* Add your mobile menu button/icon here */}
-          </div>
-          <div className="mobileMenu">
-            <a href="#home">Home</a>
-            <a href="#services">Services</a>
-            <a href="#about">About</a>
-            <a href="#contact">Contact</a>
-          </div>
-        </div>
-      </div>
+        // <---1 Wrap this component with a context
+        <div className="navbar">{<NavbarMenu />}</div>
+        // 1--->
     );
-  };
-  
-  export default Navbar;
+};
+
+export default Navbar;
