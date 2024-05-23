f45f029420a41c38937f7bc6b7edc44c80c1fa44
indianaea <126889057+indianaea@users.noreply.github.com>
2024-05-23 19:53:30 +0000

    navbar breytingar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -4,32 +4,31 @@ import "./navbar.css"
 import Link from "next/link";
 
 
-export const NavbarMenu = () => {
-    return (
-        <nav>
-            <Link href="/" className="Logo">Website</Link>
-            <ul>
-            <li>
-            <Link href="/order">Order dish</Link>
-            </li>
-            <li>
-            <Link href="/aboutUs">About us</Link>
-            </li>
-            <li>
-            <Link href="/menu">Menu</Link>
-            </li>
-            </ul>
-        </nav>
-        )
-};
-
-
 const Navbar = () => {
     return (
-        // <---1 Wrap this component with a context
-        <div className="navbar">{<NavbarMenu />}</div>
-        // 1--->
+      <div className="navbar">
+        <div className="container">
+          <div className="logo">
+            <img src="lilbits.png" alt="Logo" />
+          </div>
+          <div className="desktopMenu">
+            <a href="#home">Home</a>
+            <a href="#services">Services</a>
+            <a href="#about">About</a>
+            <a href="#contact">Contact</a>
+          </div>
+          <div className="menuBtn">
+            {/* Add your mobile menu button/icon here */}
+          </div>
+          <div className="mobileMenu">
+            <a href="#home">Home</a>
+            <a href="#services">Services</a>
+            <a href="#about">About</a>
+            <a href="#contact">Contact</a>
+          </div>
+        </div>
+      </div>
     );
-};
-
-export default Navbar;
+  };
+  
+  export default Navbar;
