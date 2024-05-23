33fbee7b3e3bc3d7b302e1ef62cdc74cae2ff446
indianaea <126889057+indianaea@users.noreply.github.com>
2024-05-23 22:39:04 +0000

    vinna í hero banner

---
+++ src/app/components/heroSlider.tsx
@@ -0,0 +1,42 @@
+"use client";
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import './heroSlider.css';
+
+const HeroSlider = () => {
+    return (
+        <div className="carousel-wrapper">
+            <Carousel
+                showArrows={false} // Hide arrows
+                showStatus={false} // Hide status
+                showIndicators={true} // Show dots
+                infiniteLoop={true}
+                showThumbs={false} // Hide thumbnails
+                useKeyboardArrows={true}
+                autoPlay={true} // Enable autoplay
+                stopOnHover={false} // Do not stop on hover
+                swipeable={true}
+                dynamicHeight={true}
+                emulateTouch={true}
+                autoFocus={false}
+                interval={3000} // Set autoplay interval
+                transitionTime={500} // Set transition time
+                swipeScrollTolerance={5}
+                ariaLabel="Hero Slider"
+            >
+                <div>
+                    <img src="lilbits.png" alt="Slide 1" />
+                </div>
+                <div>
+                    <img src="lilbits.png" alt="Slide 2" />
+                </div>
+                <div>
+                    <img src="lilbits.png" alt="Slide 3" />
+                </div>
+            </Carousel>
+        </div>
+    );
+};
+
+export default HeroSlider;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,13 @@
 import { useEffect, useState } from "react";
 import Link from "next/link";
 import Navbar from "./components/navbar";
-
+import HeroSlider from "./components/heroSlider"; // Adjust the path as necessary
 
 export default function FrontPage() {
   return (
     <>
       <Navbar />
+      <HeroSlider />
     </>
   );
 }
