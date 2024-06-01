9697d40ed734dc8b7104d9d01d6a9c1089cb736f
indianaea <126889057+indianaea@users.noreply.github.com>
2024-06-01 11:57:27 +0000

    footer

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -2,11 +2,13 @@
 import Image from "next/image";
 import Routers from "../routers";
 import Navbar from "../components/navbar";
+import Footer from "../../components/footer";
 
 export default function Menu() {
   return (
     <>
       <Navbar />
+      <Footer />
     </>
   );
 }
