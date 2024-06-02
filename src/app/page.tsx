bc6e8a11c7b38d7fde0dffa639f2cb3f54985994
indianaea <126889057+indianaea@users.noreply.github.com>
2024-06-02 10:54:12 +0000

    frontpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,12 @@ import Link from "next/link";
 import Navbar from "./components/navbar";
 import HeroSlider from "./components/heroSlider"; // Adjust the path as necessary
 import Footer from "./components/footer"; // Adjust the path as necessary
+import Button from "./components/button"; // Adjust the path as necessary
 import './common.css';  
+
+const nextPage = async () => {
+  window.location.href = "/order";
+};
 
 export default function FrontPage() {
   return (
@@ -15,6 +20,7 @@ export default function FrontPage() {
         </div>
         <div className="page-main">
           <HeroSlider />
+          <Button onClick={nextPage} caption="Order"/> 
         </div>
         <div className="page-footer">
           <Footer />
