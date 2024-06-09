075137da84d22d6b8fb4e64593517dc7170ee95a
indianaea <126889057+indianaea@users.noreply.github.com>
2024-06-09 14:46:18 +0000

    setja inn css breytingar

--- src/app/order/drink/drink.tsx
+++ src/app/order/drink/drink.tsx
@@ -26,11 +26,6 @@ const DrinkSelector: React.FC = () => {
     fetchDrinkList();
   }, []);
 
-  const getRandomBorderClass = () => {
-    const borderClasses = ["yellow-border", "green-border", "red-border"];
-    return borderClasses[Math.floor(Math.random() * borderClasses.length)];
-  };
-
   const toggleDrinkSelection = (id: number) => {
     setSelectedDrinks(prevSelected => {
       const newSelected = prevSelected.some(drink => drink.id === id)
@@ -43,7 +38,6 @@ const DrinkSelector: React.FC = () => {
   };
 
   const confirmSelection = () => {
-    //alert(`You have confirmed your selection: ${JSON.stringify(selectedDrinks)}`);
     window.location.href = "/order/confirm";
   };
 
@@ -54,14 +48,18 @@ const DrinkSelector: React.FC = () => {
           <div
             key={drink.id}
             className={`drink ${selectedDrinks.some(d => d.id === drink.id) ? 'selected' : ''}`}
-            onClick={() => toggleDrinkSelection(drink.id)}
           >
-            <img src={drink.imageSource} alt={drink.name} className={getRandomBorderClass()} />
-            <p>{drink.name}</p>
-            <p>Price: {drink.price.toFixed(0)}</p>
-            {selectedDrinks.some(d => d.id === drink.id) && (
-              <span className="checkmark">✔️</span>
-            )}
+            <img src={drink.imageSource} alt={drink.name} />
+            <div style={{ paddingTop: '80px' }}> {/* Adjust padding for content below the image */}
+              <p>{drink.name}</p>
+              <p className="price">Price: {drink.price} kr</p>
+              <button
+                className={`button ${selectedDrinks.some(d => d.id === drink.id) ? 'selected' : ''}`}
+                onClick={() => toggleDrinkSelection(drink.id)}
+              >
+                Buy
+              </button>
+            </div>
           </div>
         ))}
       </div>
