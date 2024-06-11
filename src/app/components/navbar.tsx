c49537e1d6a7b9e9044a4e5d5654bd272ae543b1
indianaea <126889057+indianaea@users.noreply.github.com>
2024-06-11 18:26:22 +0000

    breytingar á navbar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,8 +1,7 @@
 "use client";
 import { useState } from "react";
-import "./navbar.css"
+import "./navbar.css";
 import Link from "next/link";
-
 
 export const NavbarMenu = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -12,35 +11,53 @@ export const NavbarMenu = () => {
     };
 
     return (
-        <div className="container">
-            <Link href="/" className="logo">
-                <img src="/lilbits.png" alt="Website Logo" />
-            </Link>
-            <button className="menuBtn" onClick={toggleMenu}>
-                <img src="/hamburger-icon.svg" alt="Menu" className="icon" />
-            </button>
-            <ul className={`desktopMenu ${isOpen ? "mobileMenu" : ""}`}>
-                <li>
-                    <Link href="/order">Order dish</Link>
-                </li>
-                <li>
-                    <Link href="/aboutUs">About us</Link>
-                </li>
-                <li>
-                    <Link href="/menu">Menu</Link>
-                </li>
-                <li>
-                    <Link href="/order/drink">Drinks</Link>
-                </li>
-            </ul>
-        </div>
+        <>
+            <div className="navbar">
+                <div className="container">
+                    <Link href="/" className="logo">
+                        <img src="/lilbits.png" alt="Website Logo" />
+                    </Link>
+                    <button className="menuBtn" onClick={toggleMenu}>
+                        <img src="/hamburger-icon.svg" alt="Menu" className="icon" />
+                    </button>
+                    <ul className="desktopMenu">
+                        <li>
+                            <Link href="/order">Order dish</Link>
+                        </li>
+                        <li>
+                            <Link href="/aboutUs">About us</Link>
+                        </li>
+                        <li>
+                            <Link href="/menu">Menu</Link>
+                        </li>
+                        <li>
+                            <Link href="/order/drink">Drinks</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div className={`mobileMenuWrapper ${isOpen ? "open" : ""}`}>
+                <ul className="mobileMenu">
+                    <li>
+                        <Link href="/order">Order dish</Link>
+                    </li>
+                    <li>
+                        <Link href="/aboutUs">About us</Link>
+                    </li>
+                    <li>
+                        <Link href="/menu">Menu</Link>
+                    </li>
+                    <li>
+                        <Link href="/order/drink">Drinks</Link>
+                    </li>
+                </ul>
+            </div>
+        </>
     );
 };
 
 const Navbar = () => {
-    return (
-        <div className="navbar">{<NavbarMenu />}</div>
-    );
+    return <NavbarMenu />;
 };
 
 export default Navbar;
